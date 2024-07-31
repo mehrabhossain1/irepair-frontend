ef29a116c411a4d85e5d21df43d7952bd496f888
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-07-31 14:58:33 +0600

    battery replacement

--- src/components/ServiceTiles/BatteryReplacement.tsx
+++ src/components/ServiceTiles/BatteryReplacement.tsx
@@ -1,15 +1,28 @@
 import useScrollGrow from "@/hooks/ScrollGrowHook";
 import { motion } from "framer-motion";
+import bg from "@/assets/images/trackpad.png";
 
 const BatteryReplacement = () => {
-  const { style, componentRef } = useScrollGrow();
+  // const { style, componentRef } = useScrollGrow();
+
+  const myStyle = {
+    backgroundImage: `url(${bg})`,
+
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+  };
 
   return (
     <motion.div
-      style={style}
-      ref={componentRef}
-      className="bg-red-500 h-[415px] rounded-2xl col-span-12"
-    ></motion.div>
+      style={myStyle}
+      // ref={componentRef}
+      className=" h-[415px] rounded-2xl col-span-12 object-contain"
+    >
+      <p>Battery replacement</p>
+      <p>
+        Get back to 100% battery health. With 6 month replacement warranty.{" "}
+      </p>
+    </motion.div>
   );
 };
 
