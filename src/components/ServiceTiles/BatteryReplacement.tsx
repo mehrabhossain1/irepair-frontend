3a84bc32786ebaa541354480b84f61fab311813f
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-08-01 11:54:01 +0600

    battery replacement sec

--- src/components/ServiceTiles/BatteryReplacement.tsx
+++ src/components/ServiceTiles/BatteryReplacement.tsx
@@ -3,25 +3,27 @@ import { motion } from "framer-motion";
 import bg from "@/assets/images/trackpad.png";
 
 const BatteryReplacement = () => {
-  // const { style, componentRef } = useScrollGrow();
+  const { style, componentRef } = useScrollGrow();
 
   const myStyle = {
     backgroundImage: `url(${bg})`,
-
+    height: "100%",
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
   };
 
   return (
     <motion.div
-      style={myStyle}
-      // ref={componentRef}
-      className=" h-[415px] rounded-2xl col-span-12 object-contain"
+      style={style}
+      ref={componentRef}
+      className="bg-light-gray h-[415px] rounded-2xl col-span-12 "
     >
-      <p>Battery replacement</p>
-      <p>
-        Get back to 100% battery health. With 6 month replacement warranty.{" "}
-      </p>
+      <div style={myStyle} className=" rounded-2xl p-10">
+        <p className="font-semibold text-xl mb-28">Battery replacement</p>
+        <p className="text-4xl place-content-center w-1/2">
+          Get back to 100% battery health. With 6 month replacement warranty.{" "}
+        </p>
+      </div>
     </motion.div>
   );
 };
--- src/components/ServiceTiles/DataRecovery.tsx
+++ src/components/ServiceTiles/DataRecovery.tsx
@@ -1,15 +1,30 @@
 import useScrollGrow from "@/hooks/ScrollGrowHook";
 import { motion } from "framer-motion";
+import bg from "@/assets/images/trackpad.png";
 
 const DataRecovery = () => {
   const { style, componentRef } = useScrollGrow();
+
+  const myStyle = {
+    backgroundImage: `url(${bg})`,
+    height: "100%",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+  };
 
   return (
     <motion.div
       style={style}
       ref={componentRef}
-      className="bg-red-500 h-[415px] rounded-2xl col-span-6 lg:col-span-7"
-    ></motion.div>
+      className="bg-light-gray h-[415px] rounded-2xl col-span-6 lg:col-span-7"
+    >
+      <div style={myStyle} className=" rounded-2xl ">
+        <p>Battery replacement</p>
+        <p>
+          Get back to 100% battery health. With 6 month replacement warranty.{" "}
+        </p>
+      </div>
+    </motion.div>
   );
 };
 
