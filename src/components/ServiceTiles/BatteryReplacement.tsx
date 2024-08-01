aef25f2e5c5ce5ca8f7aad19c337fce6ad008089
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-08-01 12:10:01 +0600

    bg img: working on it

--- src/components/ServiceTiles/BatteryReplacement.tsx
+++ src/components/ServiceTiles/BatteryReplacement.tsx
@@ -8,22 +8,29 @@ const BatteryReplacement = () => {
   const myStyle = {
     backgroundImage: `url(${bg})`,
     height: "100%",
+    width: "628px",
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
+    backgroundPosition: "center right", // Ensure it's aligned to the right
+    marginLeft: "auto", // Push it to the end
   };
 
   return (
     <motion.div
       style={style}
       ref={componentRef}
-      className="bg-light-gray h-[415px] rounded-2xl col-span-12 "
+      className="bg-light-gray h-[415px] rounded-2xl col-span-12 flex"
     >
-      <div style={myStyle} className=" rounded-2xl p-10">
-        <p className="font-semibold text-xl mb-28">Battery replacement</p>
-        <p className="text-4xl place-content-center w-1/2">
-          Get back to 100% battery health. With 6 month replacement warranty.{" "}
+      {/* Text Section */}
+      <div className="flex-1 p-10">
+        <p className="font-semibold text-xl mb-10">Battery replacement</p>
+        <p className="text-4xl w-full">
+          Get back to 100% battery health. With 6 month replacement warranty.
         </p>
       </div>
+
+      {/* Image Section */}
+      <div style={myStyle} className="rounded-2xl"></div>
     </motion.div>
   );
 };
